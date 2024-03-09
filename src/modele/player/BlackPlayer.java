2a44948de99b320b1b5dee7bc1beb4f90ea3f0cb
hlothaire <lothaire.holcvart78@gmail.com>
2024-03-09 16:19:47 +0100

    small changes in BoardUtils and Kingcastle

--- src/modele/player/BlackPlayer.java
+++ src/modele/player/BlackPlayer.java
@@ -54,7 +54,10 @@ public class BlackPlayer extends Player{
                     && !this.board.getTile(2).isTileOccupied()
                     && !this.board.getTile(3).isTileOccupied()){
                 final Tile rookTile = this.board.getTile(0);
-                if(rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()){
+                if(rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove() &&
+                Player.calculateAttackOnTile(2,opponentLegals).isEmpty() &&
+                Player.calculateAttackOnTile(3,opponentLegals).isEmpty() &&
+                rookTile.getPiece().getPieceType().isRook()){
                     kingCastles.add(new QueenSideCastleMove(this.board,this.playerKing,2,
                             (Rook)rookTile.getPiece(),rookTile.getTileCoordinate(),3));
                 }
